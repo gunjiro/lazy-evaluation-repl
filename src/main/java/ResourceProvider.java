ecfe4b3bdc012d7bc54ce2d3e9d1cda54c2af370
Gunjiro <gunjiro.san@gmail.com>
2022-11-09 02:16:21 +0000

    Create ResourceProvider

---
+++ src/main/java/ResourceProvider.java
@@ -0,0 +1,19 @@
+import java.io.Reader;
+
+public interface ResourceProvider {
+    public Reader open(String name) throws FailedException;
+
+    public static class FailedException extends Exception {
+        public FailedException(String message) {
+            super(message);
+        }
+
+        public FailedException(Throwable cause) {
+            super(cause);
+        }
+
+        public FailedException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
