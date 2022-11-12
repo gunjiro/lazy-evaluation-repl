0e9d4d32ee6e295151c8bd521361c85aacd2a6f2
Gunjiro <gunjiro.san@gmail.com>
2022-11-12 06:15:22 +0000

    Update RequestOperator

--- src/main/java/RequestOperator.java
+++ src/main/java/RequestOperator.java
@@ -1,12 +1,12 @@
 public class RequestOperator {
-    private final CommandAnalyzer commandAnalyzer;
-    private final CommandOperator commandOperator;
-    private final EvaluationRequestAction evalAction;
+    private final RequestActionFactory factory;
 
-    public RequestOperator(CommandAnalyzer commandAnalyzer, CommandOperator commandOperator, EvaluationRequestAction evalAction) {
-        this.commandAnalyzer = commandAnalyzer;
-        this.commandOperator = commandOperator;
-        this.evalAction = evalAction;
+    private RequestOperator(RequestActionFactory factory) {
+        this.factory = factory;
+    }
+
+    public static RequestOperator create() {
+        return new RequestOperator(new RequestActionFactory());
     }
 
     public void operate(Request request) throws ExitException {
@@ -18,19 +18,13 @@ public class RequestOperator {
 
             @Override
             public Void visit(CommandRequest request) throws ExitException {
-                request.extract(new CommandRequest.Operation<Void>() {
-                    @Override
-                    public Void apply(Environment environment, String input) throws ExitException {
-                        commandOperator.operate(environment, commandAnalyzer.analyze(input));
-                        return null;
-                    }
-                });
+                factory.createCommandRequestAction().take(request);
                 return null;
             }
 
             @Override
             public Void visit(EvaluationRequest request) {
-                evalAction.take(request);
+                factory.createEvaluationRequestAction().take(request);
                 return null;
             }
         });
--- src/main/java/RequestOperatorFactory.java
+++ src/main/java/RequestOperatorFactory.java
@@ -1,26 +1,5 @@
 public class RequestOperatorFactory {
     public RequestOperator create() {
-        final MessagePrinter messagePrinter = messagePrinter();
-        return new RequestOperator(new CommandAnalyzer(), commandOperator(messagePrinter), evalAction(messagePrinter));
-    }
-
-    private MessagePrinter messagePrinter() {
-        return new SystemOutMessagePrinter();
-    }
-
-    private CommandOperator commandOperator(MessagePrinter messagePrinter) {
-        return new CommandOperator(loadAction(messagePrinter), messagePrinter);
-    }
-
-    private LoadAction loadAction(MessagePrinter messagePrinter) {
-        return new LoadAction(new FileResourceProvider(), messagePrinter);
-    }
-
-    private EvaluationRequestAction evalAction(MessagePrinter messagePrinter) {
-        return new EvaluationRequestAction(valuePrinter(), messagePrinter);
-    }
-
-    private ValuePrinter valuePrinter() {
-        return new ValuePrinter(new SystemOutStringPrinter());
+        return RequestOperator.create();
     }
 }
