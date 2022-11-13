1ea4fa1a10baed5fd5095898b83ebaa60a58bd36
Gunjiro <gunjiro.san@gmail.com>
2022-11-13 03:01:41 +0000

    Update RequestOperator

--- src/main/java/AppExecutor.java
+++ src/main/java/AppExecutor.java
@@ -1,6 +1,6 @@
 public class AppExecutor implements Executor {
     @Override
     public void execute(Environment environment, Request request) throws ExitException {
-        RequestOperator.create(AppCommandOperator.create(new FileResourceProvider(), new SystemOutMessagePrinter()), new SystemOutStringPrinter(), new SystemOutMessagePrinter()).operate(environment, request);
+        AppRequestOperator.create(AppCommandOperator.create(new FileResourceProvider(), new SystemOutMessagePrinter()), new SystemOutStringPrinter(), new SystemOutMessagePrinter()).operate(environment, request);
     }
 }
---
+++ src/main/java/AppRequestOperator.java
@@ -0,0 +1,40 @@
+public class AppRequestOperator implements RequestOperator {
+    private final RequestActionFactory factory;
+    private final CommandOperator operator;
+    private final StringPrinter strinngPrinter;
+    private final MessagePrinter messagePrinter;
+
+    private AppRequestOperator(RequestActionFactory factory, CommandOperator operator, StringPrinter strinngPrinter,
+            MessagePrinter messagePrinter) {
+        this.factory = factory;
+        this.operator = operator;
+        this.strinngPrinter = strinngPrinter;
+        this.messagePrinter = messagePrinter;
+    }
+
+    public static RequestOperator create(CommandOperator operator, StringPrinter strinngPrinter, MessagePrinter messagePrinter) {
+        return new AppRequestOperator(new RequestActionFactory(), operator, strinngPrinter, messagePrinter);
+    }
+
+    @Override
+    public void operate(Environment environment, Request request) throws ExitException {
+        request.accept(new Request.Visitor<Void>() {
+            @Override
+            public Void visit(EmptyRequest request) {
+                return null;
+            }
+
+            @Override
+            public Void visit(CommandRequest request) throws ExitException {
+                factory.createCommandRequestAction(operator).take(environment, request);
+                return null;
+            }
+
+            @Override
+            public Void visit(EvaluationRequest request) {
+                factory.createEvaluationRequestAction(strinngPrinter, messagePrinter).take(environment, request);
+                return null;
+            }
+        });
+    }
+}
