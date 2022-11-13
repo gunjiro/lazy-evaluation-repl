f8552a728b655ff79599a983b3e9f8f0a5cb2819
Gunjiro <gunjiro.san@gmail.com>
2022-11-13 03:47:23 +0000

    Remove Executor

--- src/main/java/AppExecutor.java
+++
@@ -1,6 +0,0 @@
-public class AppExecutor implements Executor {
-    @Override
-    public void execute(Environment environment, Request request) throws ExitException {
-        AppRequestOperator.create(new FileResourceProvider(), new SystemOutStringPrinter(), new SystemOutMessagePrinter()).operate(environment, request);
-    }
-}
