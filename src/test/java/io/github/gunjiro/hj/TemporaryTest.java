a1ac8423dc1616ba06f6d786d685b0f47ad82fd9
Gunjiro <gunjiro.san@gmail.com>
2024-03-12 13:05:16 +0000

    Create TemporaryTest

---
+++ src/test/java/io/github/gunjiro/hj/TemporaryTest.java
@@ -0,0 +1,12 @@
+package io.github.gunjiro.hj;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class TemporaryTest {
+    @Test
+    public void test() {
+    }
+}
