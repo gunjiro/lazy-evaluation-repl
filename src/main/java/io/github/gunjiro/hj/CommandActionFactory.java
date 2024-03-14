bc245b22e8ca2031e1057b5984b8355a4de4d8ac
Gunjiro <gunjiro.san@gmail.com>
2024-03-14 12:56:09 +0000

    Remove CommandActionFactory

--- src/main/java/io/github/gunjiro/hj/CommandActionFactory.java
+++
@@ -1,9 +0,0 @@
-package io.github.gunjiro.hj;
-
-import io.github.gunjiro.hj.command.action.QuitCommandAction;
-
-public class CommandActionFactory {
-    public QuitCommandAction createQuitCommandAction() {
-        return new QuitCommandAction();
-    }
-}
