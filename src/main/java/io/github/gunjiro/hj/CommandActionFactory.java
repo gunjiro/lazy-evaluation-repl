62dd4c75254a5425f5fa58dba05d4fab7c7e4cc6
Gunjiro <gunjiro.san@gmail.com>
2024-03-14 12:50:30 +0000

    Update CommandActionFactory

--- src/main/java/io/github/gunjiro/hj/CommandActionFactory.java
+++ src/main/java/io/github/gunjiro/hj/CommandActionFactory.java
@@ -1,15 +1,9 @@
 package io.github.gunjiro.hj;
 
-import io.github.gunjiro.hj.command.action.LoadCommandAction;
 import io.github.gunjiro.hj.command.action.QuitCommandAction;
 
 public class CommandActionFactory {
     public QuitCommandAction createQuitCommandAction() {
         return new QuitCommandAction();
     }
-
-    public LoadCommandAction createLoadCommandAction(ResourceProvider provider, LoadCommandAction.Implementor implementor) {
-        return new LoadCommandAction(provider, implementor);
-    }
-
 }
