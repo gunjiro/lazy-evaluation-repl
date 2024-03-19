1f9432510f42769b682c7b0a01f9ba575f234cad
Gunjiro <gunjiro.san@gmail.com>
2024-03-19 13:07:48 +0000

    Update RequestActionFactory

--- src/main/java/io/github/gunjiro/hj/RequestActionFactory.java
+++ src/main/java/io/github/gunjiro/hj/RequestActionFactory.java
@@ -7,22 +7,6 @@ public class RequestActionFactory {
         return new EvaluationRequestAction(new ValuePrinter(stringPrinter), messagePrinter);
     }
 
-    public CommandRequestAction createCommandRequestAction(ResourceProvider provider, MessagePrinter printer) {
-        return new CommandRequestAction(new CommandAnalyzer(), AppCommandOperator.create(provider, new AppCommandOperator.Implementor() {
-
-            @Override
-            public void showMessage(String message) {
-                printer.printMessage(message);
-            }
-
-            @Override
-            public void load(String name) {
-                throw new UnsupportedOperationException("Unimplemented method 'load'");
-            }
-            
-        }));
-    }
-
     public CommandRequestAction createCommandRequestAction(ResourceProvider provider, MessagePrinter printer, AppCommandOperator.Implementor implementor) {
         return new CommandRequestAction(new CommandAnalyzer(), AppCommandOperator.create(provider, implementor));
     }
