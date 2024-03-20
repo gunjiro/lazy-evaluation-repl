3671ba47117787735ce34810c304423d0d7cd55c
Gunjiro <gunjiro.san@gmail.com>
2024-03-20 04:51:07 +0000

    Update RequestActionFactory

--- src/main/java/io/github/gunjiro/hj/RequestActionFactory.java
+++ src/main/java/io/github/gunjiro/hj/RequestActionFactory.java
@@ -1,13 +1,7 @@
 package io.github.gunjiro.hj;
 
-import io.github.gunjiro.hj.command.CommandAnalyzer;
-
 public class RequestActionFactory {
     public EvaluationRequestAction createEvaluationRequestAction(StringPrinter stringPrinter, MessagePrinter messagePrinter) {
         return new EvaluationRequestAction(new ValuePrinter(stringPrinter), messagePrinter);
-    }
-
-    public CommandRequestAction createCommandRequestAction(AppCommandOperator.Implementor implementor) {
-        return new CommandRequestAction(new CommandAnalyzer(), new AppCommandOperator(implementor));
     }
 }
