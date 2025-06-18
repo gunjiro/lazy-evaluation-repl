91121d58e77eaf795c3e000695b4ae7e13fd4276
Gunjiro <gunjiro.san@gmail.com>
2025-06-18 13:38:35 +0900

    Add getState to ManagingStateUnit

--- src/main/java/io/github/gunjiro/hj/unit/ManagingStateUnit.java
+++ src/main/java/io/github/gunjiro/hj/unit/ManagingStateUnit.java
@@ -1,5 +1,8 @@
 package io.github.gunjiro.hj.unit;
 
+import io.github.gunjiro.hj.state.State;
+
 public abstract class ManagingStateUnit {
     public abstract void stopApplication();
+    public abstract State getState();
 }
