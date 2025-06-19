32749b0116ebcf78ca979e100eafae0bfed3e9b9
Gunjiro <gunjiro.san@gmail.com>
2025-06-20 08:16:10 +0900

    Modify OutputOperation

--- src/main/java/io/github/gunjiro/hj/ui/OutputOperation.java
+++ src/main/java/io/github/gunjiro/hj/ui/OutputOperation.java
@@ -1,16 +1,21 @@
 package io.github.gunjiro.hj.ui;
 
+import io.github.gunjiro.hj.app.AppTextOutputUnit;
+import io.github.gunjiro.hj.unit.TextOutputUnit;
+
 public class OutputOperation {
+    private final TextOutputUnit unit = new AppTextOutputUnit();
 
     public void printMessage(String message) {
-        System.out.println(message);
+        unit.output(message);
+        unit.newline();
     }
 
     public void printText(String text) {
-        System.out.print(text);
+        unit.output(text);
     }
 
     public void startANewLine() {
-        System.out.println();
+        unit.newline();
     }
 }
