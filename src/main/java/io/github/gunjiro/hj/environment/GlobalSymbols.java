4761b84df78d3eac2d289b0f840dcee6fb33788c
Gunjiro <gunjiro.san@gmail.com>
2025-06-19 14:20:08 +0900

    Create GlobalSymbols

---
+++ src/main/java/io/github/gunjiro/hj/environment/GlobalSymbols.java
@@ -0,0 +1,10 @@
+package io.github.gunjiro.hj.environment;
+
+public class GlobalSymbols {
+    private final String[] symbols;
+
+    public GlobalSymbols(String[] symbols) {
+        this.symbols = symbols;
+    }
+
+}
