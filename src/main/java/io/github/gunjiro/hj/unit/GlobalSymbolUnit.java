59a9bb523cfdcdc5ae1119199457cc9357ed6681
Gunjiro <gunjiro.san@gmail.com>
2025-06-19 14:24:41 +0900

    Modify GlobalSymbolUnit

--- src/main/java/io/github/gunjiro/hj/unit/GlobalSymbolUnit.java
+++ src/main/java/io/github/gunjiro/hj/unit/GlobalSymbolUnit.java
@@ -1,5 +1,8 @@
 package io.github.gunjiro.hj.unit;
 
+import io.github.gunjiro.hj.environment.GlobalSymbols;
+
 public abstract class GlobalSymbolUnit {
-    public abstract void update();
+    public abstract void update(GlobalSymbols symbols);
+    public abstract GlobalSymbols getGlobalSymbols();
 }
