9b4e5c1eb05f36b913518fb946aee6bbc4c39bdc
Gunjiro <gunjiro.san@gmail.com>
2025-06-21 07:18:53 +0900

    Create GeneralOperator

---
+++ src/main/java/io/github/gunjiro/hj/repl/GeneralOperator.java
@@ -0,0 +1,14 @@
+package io.github.gunjiro.hj.repl;
+
+public class GeneralOperator {
+    private final Implementor implementor;
+
+    public GeneralOperator(Implementor implementor) {
+        this.implementor = implementor;
+    }
+
+    public static interface Implementor {
+        
+    }
+
+}
