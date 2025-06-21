feb7a3e479d56e9f25c46e1fd332e4f135717f76
Gunjiro <gunjiro.san@gmail.com>
2025-06-21 11:01:29 +0900

    Modify GeneralOperator.Implementor

--- src/main/java/io/github/gunjiro/hj/repl/GeneralOperator.java
+++ src/main/java/io/github/gunjiro/hj/repl/GeneralOperator.java
@@ -1,5 +1,9 @@
 package io.github.gunjiro.hj.repl;
 
+import io.github.gunjiro.hj.unit.ManagingStateUnit;
+import io.github.gunjiro.hj.unit.TextOutputUnit;
+import io.github.gunjiro.hj.unit.ThunkTableUnit;
+
 public class GeneralOperator {
     private final Implementor implementor;
 
@@ -8,7 +12,9 @@ public class GeneralOperator {
     }
 
     public static interface Implementor {
-        
+        public TextOutputUnit createTextOutputUnit();
+        public ManagingStateUnit createManagingStateUnit();
+        public ThunkTableUnit createThunkTableUnit();
     }
 
 }
