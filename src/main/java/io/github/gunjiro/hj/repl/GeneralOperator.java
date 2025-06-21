7daf2b481cb3f9d3d78d81b0b5c55ee5e77c2b31
Gunjiro <gunjiro.san@gmail.com>
2025-06-21 11:45:46 +0900

    Modify GeneralOperator

--- src/main/java/io/github/gunjiro/hj/repl/GeneralOperator.java
+++ src/main/java/io/github/gunjiro/hj/repl/GeneralOperator.java
@@ -1,5 +1,14 @@
 package io.github.gunjiro.hj.repl;
 
+import java.io.Reader;
+import java.io.StringReader;
+
+import io.github.gunjiro.hj.AppRequestOperator;
+import io.github.gunjiro.hj.ApplicationException;
+import io.github.gunjiro.hj.Request;
+import io.github.gunjiro.hj.RequestFactory;
+import io.github.gunjiro.hj.Thunk;
+import io.github.gunjiro.hj.processor.FileLoader;
 import io.github.gunjiro.hj.unit.ManagingStateUnit;
 import io.github.gunjiro.hj.unit.TextOutputUnit;
 import io.github.gunjiro.hj.unit.ThunkTableUnit;
@@ -7,14 +16,93 @@ import io.github.gunjiro.hj.unit.ThunkTableUnit;
 public class GeneralOperator {
     private final Implementor implementor;
 
+    private final ThunkTableUnit thunkTableUnit;
+    private final TextOutputUnit textOutputUnit;
+    private final ManagingStateUnit managingStateUnit;
+
     public GeneralOperator(Implementor implementor) {
         this.implementor = implementor;
+        this.thunkTableUnit = implementor.createThunkTableUnit();
+        this.textOutputUnit = implementor.createTextOutputUnit();
+        this.managingStateUnit = implementor.createManagingStateUnit();
     }
 
     public static interface Implementor {
         public TextOutputUnit createTextOutputUnit();
         public ManagingStateUnit createManagingStateUnit();
         public ThunkTableUnit createThunkTableUnit();
+    }
+
+    public void operate(String input) {
+        final Request request = createRequest(input);
+        createOperator().operate(request);
+    }
+
+    private Request createRequest(String input) {
+        final RequestFactory factory = new RequestFactory();
+        return factory.createRequest(input);
+    }
+
+    private FileLoader createFileLoader() {
+        return new FileLoader(new FileLoader.DefaultImplementor() {
+
+            @Override
+            public void storeFunctions(Reader reader) {
+                try {
+                    thunkTableUnit.addFunctions(reader);
+                } catch (ApplicationException e) {
+                    textOutputUnit.output(e.getMessage());
+                    textOutputUnit.newline();
+                }
+            }
+
+        });
+    }
+
+    private AppRequestOperator createOperator() {
+        final AppRequestOperator.Implementor implementor = new AppRequestOperator.Implementor() {
+
+            @Override
+            public void quit() {
+                managingStateUnit.stopApplication();
+            }
+
+            @Override
+            public void sendText(String text) {
+                textOutputUnit.output(text);
+            }
+
+            @Override
+            public void sendMessage(String message) {
+                textOutputUnit.output(message);
+                textOutputUnit.newline();
+            }
+
+            @Override
+            public void load(String name) {
+                final FileLoader loader = createFileLoader();
+                loader.addObserver(message -> {
+                    textOutputUnit.output(message);
+                    textOutputUnit.newline();
+                });
+                loader.load(name);
+            }
+
+            @Override
+            public void sendBreak() {
+                textOutputUnit.newline();
+            }
+
+        };
+        final AppRequestOperator.Factory factory = new AppRequestOperator.Factory() {
+
+            @Override
+            public Thunk createThunk(String code) throws ApplicationException {
+                return thunkTableUnit.createThunk(new StringReader(code));
+            }
+
+        };
+        return new AppRequestOperator(implementor, factory);
     }
 
 }
