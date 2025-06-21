665e062102c92607d6d82b574368f4d1682f91a5
Gunjiro <gunjiro.san@gmail.com>
2025-06-22 08:21:36 +0900

    Clean NewApp

--- src/main/java/io/github/gunjiro/hj/app/NewApp.java
+++ src/main/java/io/github/gunjiro/hj/app/NewApp.java
@@ -1,11 +1,6 @@
 package io.github.gunjiro.hj.app;
 
-import io.github.gunjiro.hj.DefaultEnvironment;
-import io.github.gunjiro.hj.Environment;
 import io.github.gunjiro.hj.REPL;
-import io.github.gunjiro.hj.unit.InputUnit;
-import io.github.gunjiro.hj.unit.ManagingStateUnit;
-import io.github.gunjiro.hj.unit.TextOutputUnit;
 
 public class NewApp {
     public static void run() {
