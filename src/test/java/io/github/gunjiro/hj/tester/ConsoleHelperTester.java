70cf9784247a4d377d1206dce8ad6c1a688cd2bb
Gunjiro <gunjiro.san@gmail.com>
2025-06-23 06:32:11 +0900

    Create ConsoleHelperTester

---
+++ src/test/java/io/github/gunjiro/hj/tester/ConsoleHelperTester.java
@@ -0,0 +1,45 @@
+package io.github.gunjiro.hj.tester;
+
+import java.io.Console;
+import java.io.IOError;
+
+import io.github.gunjiro.hj.app.ConsoleHelper;
+
+public class ConsoleHelperTester {
+    private final ConsoleHelper helper = new ConsoleHelper();
+
+    private void throwsIOErrorIfConsoleIsNull() {
+        final ConsoleHelper noConsoleHelper = new ConsoleHelper() {
+            @Override
+            protected Console getConsoleOrNull() {
+                return null;
+            }
+        };
+
+        helper.output("throws IOError if Console is null : ");
+
+        try {
+            noConsoleHelper.getConsole();
+            helper.output("!!!!! Not Throw IOError !!!!!");
+            helper.newline();
+        } catch (IOError error) {
+            helper.output("OK");
+            helper.newline();
+        }
+    }
+
+    private static void test() {
+        final ConsoleHelperTester tester = new ConsoleHelperTester();
+        tester.outputStartMessage();
+        tester.throwsIOErrorIfConsoleIsNull();
+    }
+
+    private void outputStartMessage() {
+        helper.output("----- Test of ConsoleHelper -----");
+        helper.newline();
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
