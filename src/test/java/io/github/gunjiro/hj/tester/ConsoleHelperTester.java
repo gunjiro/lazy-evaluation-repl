48c276cf7f7c80192a30751a9ff0ae1c74b7f358
Gunjiro <gunjiro.san@gmail.com>
2025-06-23 06:45:55 +0900

    Modify ConsoleHelperTester

--- src/test/java/io/github/gunjiro/hj/tester/ConsoleHelperTester.java
+++ src/test/java/io/github/gunjiro/hj/tester/ConsoleHelperTester.java
@@ -2,6 +2,7 @@ package io.github.gunjiro.hj.tester;
 
 import java.io.Console;
 import java.io.IOError;
+import java.io.IOException;
 
 import io.github.gunjiro.hj.app.ConsoleHelper;
 
@@ -28,10 +29,31 @@ public class ConsoleHelperTester {
         }
     }
 
+    private void throwsIOExceptionIfReadLineReturnsNull() {
+        final ConsoleHelper nullReadLineHelper = new ConsoleHelper() {
+            @Override
+            protected String readLine() {
+                return null;
+            }
+        };
+
+        helper.output("throws IOException if readLine returns null : ");
+
+        try {
+            nullReadLineHelper.getInput();
+            helper.output("!!!!! Not Throw IOException !!!!!");
+            helper.newline();
+        } catch (IOException e) {
+            helper.output("OK");
+            helper.newline();
+        }
+    }
+
     private static void test() {
         final ConsoleHelperTester tester = new ConsoleHelperTester();
         tester.outputStartMessage();
         tester.throwsIOErrorIfConsoleIsNull();
+        tester.throwsIOExceptionIfReadLineReturnsNull();
     }
 
     private void outputStartMessage() {
