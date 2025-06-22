b781b49272049fb352fcaade28676f833672b1c3
Gunjiro <gunjiro.san@gmail.com>
2025-06-22 23:27:30 +0900

    Modify AppInputUnit

--- src/main/java/io/github/gunjiro/hj/app/AppInputUnit.java
+++ src/main/java/io/github/gunjiro/hj/app/AppInputUnit.java
@@ -1,34 +1,15 @@
 package io.github.gunjiro.hj.app;
 
-import java.io.Console;
 import java.io.IOException;
 
 import io.github.gunjiro.hj.unit.InputUnit;
 
 public class AppInputUnit extends InputUnit {
+    private final ConsoleHelper helper = new ConsoleHelper();
+
     @Override
     public String getInput() throws IOException {
-        return readLine();
-    }
-
-    private String readLine() throws IOException {
-        final String line = getConsole().readLine("> ");
-
-        if (line == null) {
-            throw new IOException("An end of stream has been reached.");
-        }
-
-        return line;
-    }
-
-    private Console getConsole() throws IOException {
-        final Console console = System.console();
-
-        if (console == null) {
-            throw new IOException("No console device is available.");
-        }
-
-        return console;
+        return helper.getInput();
     }
 
 }
