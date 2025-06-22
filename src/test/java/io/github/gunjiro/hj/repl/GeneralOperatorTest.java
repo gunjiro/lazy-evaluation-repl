d9e26905246bc2d8842a71fa63058164290cb8a2
Gunjiro <gunjiro.san@gmail.com>
2025-06-22 10:39:49 +0900

    Create GeneralOperatorTest

---
+++ src/test/java/io/github/gunjiro/hj/repl/GeneralOperatorTest.java
@@ -0,0 +1,12 @@
+package io.github.gunjiro.hj.repl;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GeneralOperatorTest {
+    @Test
+    public void testOperate() {
+    }
+}
