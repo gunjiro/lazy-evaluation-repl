94a5f23b4a179ffcd2405ca6791371a2c9ba8ded
Gunjiro <gunjiro.san@gmail.com>
2025-06-23 16:03:44 +0900

    Create CommandRequestOperator

---
+++ src/main/java/io/github/gunjiro/hj/request/CommandRequestOperator.java
@@ -0,0 +1,9 @@
+package io.github.gunjiro.hj.request;
+
+import io.github.gunjiro.hj.CommandRequest;
+
+public class CommandRequestOperator {
+    public void operate(CommandRequest request) {
+        throw new UnsupportedOperationException();
+    }
+}
