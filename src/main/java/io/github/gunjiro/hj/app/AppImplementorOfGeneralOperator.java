445200465ad20118437d6311f2285cbc9f8e192d
Gunjiro <gunjiro.san@gmail.com>
2025-06-25 11:11:13 +0900

    Create AppImplementorOfGeneralOperator

---
+++ src/main/java/io/github/gunjiro/hj/app/AppImplementorOfGeneralOperator.java
@@ -0,0 +1,70 @@
+package io.github.gunjiro.hj.app;
+
+import java.io.FileNotFoundException;
+import java.io.Reader;
+
+import io.github.gunjiro.hj.ApplicationException;
+import io.github.gunjiro.hj.Thunk;
+import io.github.gunjiro.hj.processor.FileLoader;
+import io.github.gunjiro.hj.repl.GeneralOperator;
+
+public class AppImplementorOfGeneralOperator implements GeneralOperator.Implementor {
+    private final FileLoader loader = createFileLoader();
+    private final AppUnitContainer container;
+
+    public AppImplementorOfGeneralOperator(AppUnitContainer container) {
+        this.container = container;
+    }
+
+    @Override
+    public Thunk createThunk(Reader reader) throws ApplicationException {
+        return container.getThunkTableUnit().createThunk(reader);
+    }
+
+    @Override
+    public void output(String text) {
+        container.getTextOutputUnit().output(text);
+    }
+
+    @Override
+    public void newline() {
+        container.getTextOutputUnit().newline();
+    }
+
+    @Override
+    public void stopApplication() {
+        container.getManagingStateUnit().stopApplication();
+    }
+
+    @Override
+    public void load(String name) {
+        loader.load(name);
+    }
+
+    private FileLoader createFileLoader() {
+        final FileLoader newLoader = new FileLoader(new FileLoader.Implementor() {
+            @Override
+            public Reader open(String filename) throws FileNotFoundException {
+                return container.getFileOpenUnit().open(filename);
+            }
+
+            @Override
+            public void storeFunctions(Reader reader) {
+                try {
+                    container.getThunkTableUnit().addFunctions(reader);
+                } catch (ApplicationException e) {
+                    container.getTextOutputUnit().output(e.getMessage());
+                    container.getTextOutputUnit().newline();
+                }
+            }
+
+        });
+
+        newLoader.addObserver(message -> {
+            container.getTextOutputUnit().output(message);
+            container.getTextOutputUnit().newline();
+        });
+
+        return newLoader;
+    }
+}
