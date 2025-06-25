3e435bc96ca6bea4e115bf9e07293bb47bc458db
Gunjiro <gunjiro.san@gmail.com>
2025-06-25 10:56:03 +0900

    Modify AppImplementorOfREPL

--- src/main/java/io/github/gunjiro/hj/app/App.java
+++ src/main/java/io/github/gunjiro/hj/app/App.java
@@ -12,6 +12,6 @@ public class App {
     }
 
     private REPL createREPL() {
-        return new REPL(new AppImplementorOfREPL());
+        return new REPL(new AppImplementorOfREPL(new AppUnitContainer()));
     }
 }
--- src/main/java/io/github/gunjiro/hj/app/AppImplementorOfREPL.java
+++ src/main/java/io/github/gunjiro/hj/app/AppImplementorOfREPL.java
@@ -12,8 +12,12 @@ import io.github.gunjiro.hj.repl.REPL;
 import io.github.gunjiro.hj.state.State;
 
 public class AppImplementorOfREPL implements REPL.Implementor {
-    private final AppUnitContainer container = new AppUnitContainer();
     private final GeneralOperator operator = createGeneralOperator();
+    private final AppUnitContainer container;
+
+    public AppImplementorOfREPL(AppUnitContainer container) {
+        this.container = container;
+    }
 
     @Override
     public void operate(String input) {
