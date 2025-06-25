3b661043364daa1c7ce28ee16bb7362d688a9d4f
Gunjiro <gunjiro.san@gmail.com>
2025-06-25 11:36:09 +0900

    Create AppImplementorOfFileLoader

---
+++ src/main/java/io/github/gunjiro/hj/app/AppImplementorOfFileLoader.java
@@ -0,0 +1,31 @@
+package io.github.gunjiro.hj.app;
+
+import java.io.FileNotFoundException;
+import java.io.Reader;
+
+import io.github.gunjiro.hj.ApplicationException;
+import io.github.gunjiro.hj.processor.FileLoader;
+
+public class AppImplementorOfFileLoader implements FileLoader.Implementor {
+    private final AppUnitContainer container;
+
+    public AppImplementorOfFileLoader(AppUnitContainer container) {
+        this.container = container;
+    }
+
+    @Override
+    public Reader open(String filename) throws FileNotFoundException {
+        return container.getFileOpenUnit().open(filename);
+    }
+
+    @Override
+    public void storeFunctions(Reader reader) {
+        try {
+            container.getThunkTableUnit().addFunctions(reader);
+        } catch (ApplicationException e) {
+            container.getTextOutputUnit().output(e.getMessage());
+            container.getTextOutputUnit().newline();
+        }
+    }
+
+}
