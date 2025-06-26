ca15e683e9a02c27a83eec486dafac2ade00db33
Gunjiro <gunjiro.san@gmail.com>
2025-06-26 21:43:39 +0900

    Modify ConsoleHelperTester

--- src/test/java/io/github/gunjiro/hj/tester/ConsoleHelperTester.java
+++ src/test/java/io/github/gunjiro/hj/tester/ConsoleHelperTester.java
@@ -26,20 +26,6 @@ public class ConsoleHelperTester {
         assertThat(messages, contains("java.io.IOException: No console device is available."));
     }
 
-    private void throwsIOErrorIfConsoleIsNullOld() {
-        final ConsoleHelper helper = ConsoleHelper.create();
-        helper.output("throws IOError if Console is null : ");
-
-        try {
-            ConsoleHelper.create(null);
-            helper.output("!!!!! Not Throw IOError !!!!!");
-            helper.newline();
-        } catch (IOError error) {
-            helper.output("OK");
-            helper.newline();
-        }
-    }
-
     @Test
     public void throwsIOExceptionIfReadLineReturnsNull() {
         final Deque<String> messages = new LinkedList<>();
@@ -71,53 +57,4 @@ public class ConsoleHelperTester {
         assertThat(messages, contains("An end of stream has been reached."));
     }
 
-    private void throwsIOExceptionIfReadLineReturnsNullOld() {
-        final ConsoleHelper helper = ConsoleHelper.create();
-        final ConsoleHelper nullReadLineHelper = new ConsoleHelper(new ConsoleHelper.ConsoleEmulator() {
-
-            @Override
-            public String readLine() {
-                return null;
-            }
-
-            @Override
-            public void print(String s) {
-                throw new UnsupportedOperationException("Unimplemented method 'print'");
-            }
-
-            @Override
-            public void println() {
-                throw new UnsupportedOperationException("Unimplemented method 'println'");
-            }
-
-        });
-
-        helper.output("throws IOException if readLine returns null : ");
-
-        try {
-            nullReadLineHelper.getInput();
-            helper.output("!!!!! Not Throw IOException !!!!!");
-            helper.newline();
-        } catch (IOException e) {
-            helper.output("OK");
-            helper.newline();
-        }
-    }
-
-    private static void test() {
-        final ConsoleHelperTester tester = new ConsoleHelperTester();
-        tester.outputStartMessage();
-        tester.throwsIOErrorIfConsoleIsNullOld();
-        tester.throwsIOExceptionIfReadLineReturnsNullOld();
-    }
-
-    private void outputStartMessage() {
-        final ConsoleHelper helper = ConsoleHelper.create();
-        helper.output("----- Test of ConsoleHelper -----");
-        helper.newline();
-    }
-
-    public static void main(String[] args) {
-        test();
-    }
 }
