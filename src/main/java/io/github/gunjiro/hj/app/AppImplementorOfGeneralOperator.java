0787070d7067d0fb0104d902da9254d970217c80
Gunjiro <gunjiro.san@gmail.com>
2025-06-27 11:37:35 +0900

    Modify AppImplementorOfGeneralOperator

--- src/main/java/io/github/gunjiro/hj/app/AppImplementorOfGeneralOperator.java
+++ src/main/java/io/github/gunjiro/hj/app/AppImplementorOfGeneralOperator.java
@@ -11,7 +11,7 @@ public class AppImplementorOfGeneralOperator implements GeneralOperator.Implemen
     private final AppUnitContainer container;
     private final FileLoader loader;
 
-    public AppImplementorOfGeneralOperator(AppUnitContainer container, FileLoader loader) {
+    private AppImplementorOfGeneralOperator(AppUnitContainer container, FileLoader loader) {
         this.container = container;
         this.loader = loader;
     }
